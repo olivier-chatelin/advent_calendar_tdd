7db9623834334dfe59c93b65824f1aef84eeaea6
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-04 12:58:59 +0100

    d3/p1

--- src/Exercise.php
+++ src/Exercise.php
@@ -6,7 +6,30 @@ class Exercise
 {
     public function part1(array $data): string
     {
-        return "";
+        $total = [];
+        $gamma = [];
+        $epsilon = [];
+        $newData = [];
+        $binaryLength = strlen($data[0]);
+        foreach ($data as $digit) {
+            $newData[] = str_split($digit);
+        }
+        for($i = 0; $i < $binaryLength; $i++) {
+            $total = 0;
+            foreach ($newData as $dataLine) {
+                $total += $dataLine[$i];
+            }
+                if($total >= count($newData) / 2) {
+                    $gamma[] = 1;
+                    $epsilon[] = 0;
+                } else{
+                    $gamma[] = 0;
+                    $epsilon[] = 1;
+
+                }
+        }
+
+        return (int)bindec(implode('',$gamma)) * (int)bindec(implode('',$epsilon));
     }
 //    public function part2(array $data): string
 //    {
--- src/ExerciseTest.php
+++ src/ExerciseTest.php
@@ -9,7 +9,11 @@ class ExerciseTest extends TestCase
     public function testPart1()
     {
         $exercise = new Exercise();
-        $this->assertEquals('',$exercise->part1([]));
+        $this->assertEquals('0',$exercise->part1(['0','0','1']));
+        $this->assertEquals('0',$exercise->part1(['01','10','11']));
+        $this->assertEquals('6',$exercise->part1(['110','100','111']));
+        $this->assertEquals('198',$exercise->part1(['00100','11110','10110','10111','10101','01111','11100','10000','11001','00010','01010']));
+
     }
 //    public function testPart2()
 //    {
