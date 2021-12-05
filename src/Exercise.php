85313bc0300eea36f62f7e0b5b2e5b7030c6b642
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-05 01:15:29 +0100

    d4/p2

--- src/Exercise.php
+++ src/Exercise.php
@@ -2,12 +2,67 @@
 
 namespace App;
 
+use SebastianBergmann\CodeCoverage\Report\PHP;
+
 class Exercise
 {
     public function part1(array $data): string
     {
-        return "";
+        $randoms = explode(',',$data[0]);
+        $grids =[];
+        for ($i = 2; $i < count($data); $i += 6) {
+            $grid = [];
+            for($j = 0; $j < 5; $j++ ) {
+                $grid[] = explode(' ',$data[$j + $i]);
+            }
+            $grids[] = $grid;
+
+        }
+        $gridResults = [];
+        foreach ($randoms as $random) {
+            foreach ($grids as $gridIndex => $gridRows) {
+                foreach ($gridRows as $gridRowIndex => $gridColumns) {
+                    foreach ($gridColumns as $gridColumnIndex => $gridColumn) {
+                        if((int)$random === (int)$gridColumn) {
+                            $gridResults[$gridIndex]["row"][] = $gridRowIndex;
+                            $gridResults[$gridIndex]["col"][] = $gridColumnIndex;
+                        }
+                    }
+                }
+            }
+            foreach ($gridResults as $gridIndex => $gridResult) {
+                foreach(array_count_values( $gridResult['row']) as $row => $count) {
+                    if ($count === 5) {
+                        unset($grids[$gridIndex]);
+                        unset($gridResults[$gridIndex]);
+                        if (count( $gridResults) === 0) {
+//                            print_r($grids);
+//                            print_r($gridResults);
+
+                            print_r($random);
+                            var_dump($gridIndex);
+                            die('stop');
+                        }
+                    }
+                }
+                foreach(array_count_values( $gridResult['col']) as $col => $count) {
+                    if ($count === 5) {
+                        unset($grids[$gridIndex]);
+                        unset($gridResults[$gridIndex]);
+                        if (count( $gridResults) === 0) {
+                            print_r($random);
+                            var_dump($gridIndex);
+                            die('stop');
+                        }
+                    }
+                }
+            }
+        }
+
+    return '';
     }
+
+
 //    public function part2(array $data): string
 //    {
 //        return "";
--- src/ExerciseTest.php
+++ src/ExerciseTest.php
@@ -9,7 +9,37 @@ class ExerciseTest extends TestCase
     public function testPart1()
     {
         $exercise = new Exercise();
-        $this->assertEquals('',$exercise->part1([]));
+        $this->assertEquals([], $exercise->getMarkedCoordinates([24], [[13]]));
+        $this->assertEquals([0 => [24 => [0, 0]]], $exercise->getMarkedCoordinates([24], [[24]]));
+        $this->assertEquals([], $exercise->getMarkedCoordinates([25, 13], [[24]]));
+        $this->assertEquals([0 => [24 => [0, 0]]], $exercise->getMarkedCoordinates( [24, 13], [[24]]));
+        $this->assertEquals([
+            0 => [
+                24 => [0, 0]
+            ]
+        ],$exercise->getMarkedCoordinates([24, 13], [0=>[24], [25]]));
+        $this->assertEquals([
+            0 => [
+                24 => [0, 0]
+            ],
+            1 => [
+               13 =>[0, 0]
+            ],
+            2 => [
+                24 => [0, 0]
+            ],
+        ],$exercise->getMarkedCoordinates([24,13],[[24],[13],[24]]));
+        $this->assertEquals([
+            0 => [
+                24 => [0, 0]
+            ],
+            1 => [
+               13 =>[0, 0]
+            ],
+            2 => [
+                24 => [0, 0]
+            ],
+        ],$exercise->getMarkedCoordinates([24,13],[[24],[13],[24]]));
     }
 //    public function testPart2()
 //    {
